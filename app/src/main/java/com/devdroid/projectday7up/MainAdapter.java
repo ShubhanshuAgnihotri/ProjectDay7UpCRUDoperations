af2c1be1dbc9f87643df2e89e3776657c34c7a0b
shubh <shubhanshuagnihotri16@gmail.com>
2024-03-07 23:51:55 +0530

    ProjectDay7Up

---
+++ app/src/main/java/com/devdroid/projectday7up/MainAdapter.java
@@ -0,0 +1,163 @@
+package com.devdroid.projectday7up;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainAdapter extends FirebaseRecyclerAdapter<com.devdroid.projectday7up.MainModel,MainAdapter.myViewHolder> {
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public MainAdapter(@NonNull FirebaseRecyclerOptions<com.devdroid.projectday7up.MainModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull myViewHolder holder,final int position,@NonNull com.devdroid.projectday7up.MainModel model) {
+
+        holder.name.setText(model.getName());
+        holder.course.setText(model.getCourse());
+        holder.email.setText(model.getEmail());
+        Glide.with(holder.img.getContext())
+                .load(model.getTurl())
+                .placeholder(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark)
+                .circleCrop()
+                .error(com.google.firebase.database.R.drawable.common_google_signin_btn_icon_dark_normal)
+                .into(holder.img);
+
+        holder.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.img.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.update_popup))
+                        .setExpanded(true,1200)
+                        .create();
+
+                View view = dialogPlus.getHolderView();
+
+
+                EditText name = view.findViewById(R.id.txtName);
+                EditText course = view.findViewById(R.id.txtCourse);
+                EditText email = view.findViewById(R.id.txtEmail);
+                EditText turl = view.findViewById(R.id.txtImageUrl);
+
+                Button btnUpdate = view.findViewById(R.id.btnUpdate);
+
+                name.setText(model.getName());
+                course.setText(model.getCourse());
+                email.setText(model.getEmail());
+                turl.setText(model.getTurl());
+
+                dialogPlus.show();
+
+                btnUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v){
+                        Map<String,Object> map = new HashMap<>();
+
+                        map.put("name",name.getText().toString());
+                        map.put("course",course.getText().toString());
+                        map.put("email",email.getText().toString());
+                        map.put("turl",turl.getText().toString());
+
+                        FirebaseDatabase.getInstance().getReference().child("teachers")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.name.getContext(),"Data Updated Successfully",Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(Exception e) {
+                                        Toast.makeText(holder.name.getContext(),"Error While Updating.",Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                });
+                    }
+                });
+
+            }
+        });
+
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(holder.name.getContext());
+                builder.setTitle("Are you Sure?");
+                builder.setMessage("Deleted data cant't be Undo.");
+                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        FirebaseDatabase.getInstance().getReference().child("teachers")
+                                .child(getRef(position).getKey()).removeValue();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        Toast.makeText(holder.name.getContext(),"Cancelled",Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                builder.show();
+
+            }
+        });
+
+
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_item,parent,false);
+        return new myViewHolder(view);
+    }
+
+    class myViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView img;
+        TextView name,course,email;
+        Button btnEdit,btnDelete;
+        public myViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            name = (TextView)itemView.findViewById(R.id.nametext);
+            course = (TextView)itemView.findViewById(R.id.coursetext);
+            email = (TextView)itemView.findViewById(R.id.emailtext);
+            img = (CircleImageView)itemView.findViewById(R.id.img1);
+
+            btnEdit = (Button)itemView.findViewById(R.id.btnEdit);
+            btnDelete = (Button)itemView.findViewById(R.id.btnDelete);
+        }
+    }
+}
